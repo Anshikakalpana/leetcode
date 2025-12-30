9f129e828dbedfc2840561fb66d6a2f29c0ee906
Anshikakalpana <anshikajain196872@gmail.com>
2025-12-30 16:54:44 +0530

    Time: 67 ms (74.92%), Space: 182 MB (38.99%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,72 +1,42 @@
 class LRUCache {
-private:
-    int cap;  
-  
-
-    list<pair<int, int>> dll;  
-    
-
-    unordered_map<int, list<pair<int, int>>::iterator> mp;
-
 public:
+    int cap;
+    list<int> dll;  
+    unordered_map<int, pair<int, list<int>::iterator>> mp;
+
     LRUCache(int capacity) {
         cap = capacity;
-        
     }
 
     int get(int key) {
-        // if key not present, return -1
         if (mp.find(key) == mp.end())
             return -1;
 
-       
-        auto it = mp[key];
-        int value = it->second;
+        // move key to front
+        dll.erase(mp[key].second);
+        dll.push_front(key);
+        mp[key].second = dll.begin();
 
-        dll.erase(it);
-
-        
-        dll.push_front({key, value});
-
-        mp[key] = dll.begin();
-
-    
-        return value;
+        return mp[key].first;
     }
 
     void put(int key, int value) {
-        // if key already exists
+        // key already exists
         if (mp.find(key) != mp.end()) {
-         
-            dll.erase(mp[key]);
+            dll.erase(mp[key].second);
         }
-        // if cache is full and key is new
         else if (dll.size() == cap) {
-          
-            auto lru = dll.back();
-
-            
-            mp.erase(lru.first);
-
-            // remove LRU node from list
+            int lru = dll.back();
             dll.pop_back();
+            mp.erase(lru);
         }
 
-  //   updated key-value pair at front (MRU)
-        dll.push_front({key, value});
-
-        // store iterator of the new node in the map
-        mp[key] = dll.begin();
+        dll.push_front(key);
+        mp[key] = {value, dll.begin()};
     }
 };
 
 
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
 
 /**
  * Your LRUCache object will be instantiated and called as such:
